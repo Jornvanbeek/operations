46cfe74c80666254d6a29924700fb5dd240db606
Jarem95 <48213567+Jarem95@users.noreply.github.com>
2022-11-16 14:53:54 +0100

    Create landing_minimize_cost.py

---
+++ landing_minimize_cost.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 14:41:42 2022
+
+@author: jaremhinsenveld
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from gurobipy import Model, GRB, LinExpr
+from datetime import datetime
+
+start_time = datetime.now()
+
+"""Initiate model"""
+
+model = Model()
+
+
+# %%
+
+
+"""Model parameters"""
+
+
+# %%
+
+
+"""Variables"""
+
+
+model.update()
+
+
+# %%
+
+
+"""Constraints"""
+
+# %%
+model.update()
+
+
+# %%
+
+
+"""Objective function"""
+
+obj = 0  # Objective function (aanpassen)
+
+
+model.setObjective(obj, GRB.MINIMIZE)
+# Updating the model
+model.update()
+
+
+# %%
+
+"""Modelling stuff"""
+
+
+# Writing the .lp file. Important for debugging
+model.write('model_formulation.lp')
+
+
+model.optimize()
+
+
+# Saving our solution in the form [name of variable, value of variable]
+solution = []
+for v in model.getVars():
+    solution.append([v.varName, v.x])
+
+print(solution)
+
+
+calc_time = datetime.now()-start_time
